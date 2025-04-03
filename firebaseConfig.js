93c956f28c147703c9e9bda298f3d0c20fb80c09
omlohade <omlohade2@gmail.com>
2025-04-03 13:33:36 +0530

    new

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXgewysMxCONBO6U6A12Ib2lvbFSmeWOE",
+  authDomain: "login-page-5e28f.firebaseapp.com",
+  projectId: "login-page-5e28f",
+  storageBucket: "login-page-5e28f.appspot.com",
+  messagingSenderId: "807844115948",
+  appId: "1:807844115948:web:b98cebfcdc4c8b11f03cc9",
+  measurementId: "G-KK0E2X1V3C",
+};
+
+// Export configuration
+export default firebaseConfig;
