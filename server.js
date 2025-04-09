4fc8e759bd214857cd3c5e30475a976ba39d078e
omlohade <omlohade2@gmail.com>
2025-04-09 11:49:59 +0530

    commit

--- server.js
+++ server.js
@@ -2,16 +2,24 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
+// Get __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.join(__dirname, "public")));
+// Absolute path to the public folder
+const publicPath = path.join(__dirname, "public");
 
+// Serve static files
+app.use(express.static(publicPath));
+
+// Handle all other routes with index.html
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
