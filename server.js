7e4aed55c97424206dd53385cff71185607008c8
omlohade <omlohade2@gmail.com>
2025-04-09 11:51:43 +0530

    ok

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 import express from "express";
 import path from "path";
-import { fileURLToPath } from "url";
-
-// Get __dirname in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Absolute path to the public folder
-const publicPath = path.join(__dirname, "public");
+// Replace this with your actual absolute path to the public directory
+const publicPath = "/public"; // Railway container uses /app as root
 
-// Serve static files
 app.use(express.static(publicPath));
 
-// Handle all other routes with index.html
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
